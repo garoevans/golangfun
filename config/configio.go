85becbd559d1878818e87fe0c6b957dd8843296f
Gareth Evans <garoevans@gmail.com>
2014-03-20 08:39:06 +0000

    tabs/spaces and bits, woop

--- config/configio.go
+++ config/configio.go
@@ -1,40 +1,40 @@
 package main
 
 import (
-	"flag"
-	"os"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
+    "flag"
+    "os"
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
 )
 
 func main() {
-	key := flag.String("key", "", "A key to get")
-	val := flag.String("val", "", "Coupled with 'key' sets a new val")
-	flag.Parse()
+    key := flag.String("key", "", "A key to get")
+    val := flag.String("val", "", "Coupled with 'key' sets a new val")
+    flag.Parse()
 
-	if len(*key) == 0 {
-		flag.PrintDefaults()
-	} else {
-		file, e := ioutil.ReadFile("./config.json")
-		if e != nil {
-        fmt.Printf("File error: %v\n", e)
-        os.Exit(1)
+    if len(*key) == 0 {
+        flag.PrintDefaults()
+    } else {
+        file, e := ioutil.ReadFile("./config.json")
+        if e != nil {
+            fmt.Printf("File error: %v\n", e)
+            os.Exit(1)
+        }
+
+        var f interface{}
+        json.Unmarshal(file, &f)
+
+        if len(*val) > 0 {
+            f.(map[string]interface{})[*key] = *val
+            newF, e := json.Marshal(&f)
+            if e != nil {
+                fmt.Printf("JSON error: %v\n", e)
+                os.Exit(1)
+            }
+            ioutil.WriteFile("./config.json", newF, 0644)
+        }
+
+        fmt.Printf("%s: %s\n", *key, f.(map[string]interface{})[*key])
     }
-
-		var f interface{}
-		json.Unmarshal(file, &f)
-
-		if len(*val) > 0 {
-			f.(map[string]interface{})[*key] = *val
-			newF, e := json.Marshal(&f)
-			if e != nil {
-					fmt.Printf("JSON error: %v\n", e)
-					os.Exit(1)
-			}
-			ioutil.WriteFile("./config.json", newF, 0644)
-		}
-
-		fmt.Printf("%s: %s\n", *key, f.(map[string]interface{})[*key])
-	}
 }
